43fabdb5d0a378816400c34f8e67f218b6921feb
vinaykumar1578 <vinaykumar25202@gmail.com>
2024-11-28 18:37:52 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,36 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         
+         <p> <i class="fas fa-envelope"></i>surya2001dev@gmail.com </p>
+         <p> <i class="fas fa-envelope"></i>nitinkumarjams12@gmail.com </p>
+         <p> <i class="fas fa-envelope"></i>vinaykumar25202@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Delhi, india - 110075 </p>
+      </div>
+
+      
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span> nitin kumar</span> </p>
+
+</section>
